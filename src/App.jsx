a10a242d67a299d64a5731af6e3e84cdd560897e
Yadin Nou <nouyadin@gmail.com>
2026-09-16 00:07:47 +1000

    First commit library

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import { useState } from "react";
+import "./App.css";
+
+const App = () => {
+  return (
+    <>
+      <div>Welcome to Library Management System</div>
+    </>
+  );
+};
+
+export default App;
