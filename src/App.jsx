110ad7c8ec85f00e2236042880d386af8a347c37
Yadin Nou <nouyadin@gmail.com>
2026-09-17 23:43:19 +1000

    configure front end

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
 import { useState } from "react";
 import "./App.css";
+import { Button } from "react-bootstrap";
 
 const App = () => {
+  toasti("Wow so easy!");
   return (
     <>
       <div>Welcome to Library Management System</div>
+      <ToastContainer />
     </>
   );
 };
